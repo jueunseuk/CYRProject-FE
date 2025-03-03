fa2cf5a59f8aa3e6f02ada93f3f318b08fad20ea
jueunseuk <junsu120202@gmail.com>
2025-03-03 16:50:14 +0900

    [#setting] 프로젝트 세팅

---
+++ src/RouterList.jsx
@@ -0,0 +1,8 @@
+// src/RouterList.js
+import { createBrowserRouter } from "react-router-dom";
+
+export const RouterList = () => [
+
+];
+
+export const RouterObject = createBrowserRouter(RouterList());
