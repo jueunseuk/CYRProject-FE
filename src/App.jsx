551536bb7a89ad3a3721d8809574ff5715f6f146
returns0825 <junsu120202@gmail.com>
2025-03-24 22:11:19 +0900

    #feature: auth login with refresh token

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,46 @@ import { RecoilRoot } from 'recoil';
 import { RouterObject } from './RouterList';
 import '@/styles/index.css';
 
+const AppWithAuth = () => {
+  const setUser = useSetRecoilState(userState);
+
+    useEffect(() => {
+        const restoreLogin = async () => {
+          try {
+              const res = await axios.post(
+                  `${import.meta.env.VITE_BACKEND_BASE_URL}/auth/token/access/reset`,
+                  {},
+                  { withCredentials: true }
+            );
+
+            const accessToken = res.headers["authorization"];
+            if(accessToken) {
+              setUser({
+                  userId: res.data.userId,
+                  name: res.data.name,
+                  nickname: res.data.nickname,
+                  profileUrl: res.data.profileUrl,
+                  accessToken: accessToken,
+                  role: res.data.role,
+                });
+            }
+          } catch (error) {
+              console.log("로그인 실패 또는 비로그인 상태");
+          }
+      };
+
+      restoreLogin();
+    }, [setUser]);
+
+  return <RouterProvider router={RouterObject} />;
+};
+
 function App() {
   return (
     <StrictMode>
-        <RecoilRoot>
-          <RouterProvider router={RouterObject} />
-        </RecoilRoot>
+      <RecoilRoot>
+        <AppWithAuth />
+      </RecoilRoot>
     </StrictMode>
   );
 }
