3820e0e9719090a2005f5259c9270962b387021b
returns0825 <junsu120202@gmail.com>
2025-03-27 20:29:38 +0900

    #fix: delete to app api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
+import { RouterProvider } from 'react-router-dom';
 import '@/styles/index.css';
 
 function App() {
