0feed79900b899bead31e2ae4c98114b8946918a
returns0825 <junsu120202@gmail.com>
2025-04-07 15:57:17 +0900

    #fix: gallery rendering

--- src/components/post/galleryPost/index.jsx
+++ src/components/post/galleryPost/index.jsx
@@ -53,7 +53,9 @@ const GalleryPost = () => {
                         <S.Text $size={"14px"} style={{cursor: "pointer"}} onClick={handleNavigateGallery}>유리 갤러리 &gt;</S.Text>
                         <S.HorizontalWrapper $justify={"space-between"} style={{width: "100%"}}>
                             <S.Text $size={"18px"} $weight={"700"} style={{marginTop: "1px"}}>{formData.title}</S.Text>
-                            {user.userId === formData.authorId ? (<MoreOption formData={formData}/>) : ""}
+                            {formData.authorId !== 0 && user.userId === formData.authorId && (
+                                <MoreOption formData={formData} />
+                            )}
                         </S.HorizontalWrapper>
                         <S.HorizontalWrapper $gap={"15px"} style={{marginTop: "10px"}}>
                             <S.Profile src={formData.profileImageUrl}/>
