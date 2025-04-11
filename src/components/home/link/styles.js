625e96c78579884474fc575e5d18ff1085a593ca
returns0825 <junsu120202@gmail.com>
2025-04-11 13:11:33 +0900

    #feature: link component alter

--- src/components/home/link/styles.js
+++ src/components/home/link/styles.js
@@ -21,21 +21,68 @@ export const TitleArea = styled.div`
 export const Text = styled.div`
     font-size: ${({$size}) => $size};
     font-weight: ${({$weight}) => $weight || "400"};
-    color: ${({$color}) => $color}
+    color: ${({$color}) => $color};
 `;
 
-export const ContentArea = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 3px;
-    align-items: center;
+export const GradientText = styled.span`
+  background: linear-gradient(90deg, #FFD153, #EF4C5E, #C837AB, #5956CA);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-weight: ${({$weight}) => $weight || "400"};
+  font-size: ${({$size}) => $size};
+`;
+
+export const CotentWrapper = styled.div`
+    display: flex;
+    gap: ${({$gap}) => $gap || "0"};
+    justify-content: ${({$justify}) => $justify || "space-between"};
+    align-items: ${({$align}) => $align || "center"};
     width: 100%;
     height: 175px;
 `;
 
-export const LinkButton = styled.img`
-    width: 275px;
-    height: 40px;
-    cursor: pointer;
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: ${({$gap}) => $gap || "5px"};
+    justify-content: ${({$justify}) => $justify || "flex-start"};
+    align-items: ${({$align}) => $align || "center"};
+    width: 100%;
+    height: 175px;
+    padding: 10px 0 0 0;
+`;
+
+export const PresentImage = styled.img`
+    width: 45px;
+    heigth: 45px;
+    border-radius: 45px;
+`;
+
+export const LinkList = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    gap: 3px;
+`;
+
+export const ItemArea = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    gap: 3px;
+`;
+
+export const Icon = styled.div`
+    width: 13px;
+    height: 13px;
+    background-image: url(${props => props.$icon});
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+`;
+
+export const Link = styled.a`
+    text-decoration: none;
 `;
