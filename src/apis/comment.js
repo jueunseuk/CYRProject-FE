70c15b642a6f4c6655630aba8f9203bfd126c3b7
returns0825 <junsu120202@gmail.com>
2025-09-27 23:54:23 +0900

    #feat: comment CRUD

--- src/apis/comment.js
+++ src/apis/comment.js
@@ -42,3 +42,39 @@ export const getPostCommentList = async (postId) => {
         }
     }
 };
+
+export const patchComment = async (form, commentId) => {
+    try {
+        console.log(form)
+        const response = await instance.patch(`/comment/${commentId}`, form, {headers: {Accept: "application/json"}});
+        return response;
+    } catch(error) {
+        if(error.response && error.response.data) {
+            const errorCode = error.response.data.code;
+
+            if(errorCode === "POST_001") {
+                alert("작성자가 삭제했거나 차단된 게시글입니다.");
+            }
+
+            throw error;
+        } else {
+            alert("서버가 원활하지 않습니다.\n 다시 시도해주세요.");
+        }
+    }
+}
+
+export const deleteComment = async (commentId) => {
+    try {
+        const response = await instance.delete(`/comment/${commentId}`);
+        return response.data;
+    } catch(error) {
+        if(error.response && error.response.data) {
+            const errorCode = error.response.code;
+            if(errorCode === "POST_001") {
+                alert("작성자가 삭제했거나 차단된 게시글입니다.");
+            }
+        } else {
+            console.log("서버가 응답하지 않습니다.");
+        }
+    }
+}
--- src/components/modal/postDelete/index.jsx
+++ src/components/modal/postDelete/index.jsx
@@ -1,17 +1,21 @@
 import * as G from "@/apis/gallery";
 import * as P from "@/apis/post";
+import * as C from "@/apis/comment";
 import * as S from "./styles";
 import { useNavigate } from "react-router-dom";
 
 const DeleteModal = ({onClose, id, type}) => {
     const navigate = useNavigate();
 
-    const handleDeleteGallery = async () => {
+    const handleDelete = async () => {
         try {
             if(type === "gallery") {
                 await G.deleteGallery(id);
                 alert("성공적으로 삭제했습니다.");
                 navigate("/gallery");
+            }  else if(type === "comment") {
+                await C.deleteComment(id);
+                window.location.reload();
             } else {
                 await P.deletePost(id);
                 alert("게시글을 성공적으로 삭제했습니다.\n해당 게시판으로 이동합니다.");
@@ -26,9 +30,9 @@ const DeleteModal = ({onClose, id, type}) => {
         <>
             <S.Wrapper>
                 <S.Content>
-                    <S.Text $size={"20px"} $weight={"600"}>정말로 해당 게시글을 삭제하시겠습니까?</S.Text>
+                    <S.Text $size={"20px"} $weight={"600"}>정말로 삭제하시겠습니까?</S.Text>
                     <S.HorizontalWrapper>
-                        <S.Button $bg={"#FF3838"} style={{color: "white"}} onClick={handleDeleteGallery}>삭제</S.Button>
+                        <S.Button $bg={"#FF3838"} style={{color: "white"}} onClick={handleDelete}>삭제</S.Button>
                         <S.Button $bg={"#E7E7E7"} onClick={onClose}>취소</S.Button>
                     </S.HorizontalWrapper>
                 </S.Content>
