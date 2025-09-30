ef9af04cd3bab5f913edb485284869605343bbc5
returns0825 <junsu120202@gmail.com>
2025-09-30 15:12:02 +0900

    #fix: profile sidebar

--- src/apis/user.js
+++ src/apis/user.js
@@ -2,4 +2,20 @@ import instance from "./instance";
 
 export const requestUserExperience = () => {
     return instance.get("/user/exp");
-}
+};
+
+export const getUserSidebar = async () => {
+    try {
+        const response = await instance.get(`/user/sidebar`);
+        return response;
+    } catch (error) {
+        console.log(error)
+        const errorCode = error.response.code;
+        
+        if(errorCode === "USER_001") {
+            console.log("사용자를 찾을 수 없습니다.");
+        }
+
+        throw error;
+    }
+};
