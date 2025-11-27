00ef9cd4c7cc390e53d0a2123c56e1f50c6421f4
returns0825 <junsu120202@gmail.com>
2025-11-27 10:47:35 +0900

    #feat: loud speaker

---
+++ src/components/home/loudSpeaker/index.jsx
@@ -0,0 +1,93 @@
+import * as EP from "@/apis/exposurePost";
+import * as S from "./styles";
+import * as BC from "@/common/basic/BasicComponent";
+import loudspeaker from "@/assets/icon/post/loudspeaker.svg";
+import information from "@/assets/icon/etc/information.svg";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { formatDate } from "@/util/dateFormatter";
+import { SkeletonItem } from "@/common/skeleton/Skeleton";
+
+const LoudSpeakerPost = () => {
+    const page = 0;
+    const sort = "createdAt";
+    const [posts, setPosts] = useState([]);
+    const [skeleton, setSkeleton] = useState(true);
+    const navigate = useNavigate();
+
+    const handleNavigatePost = (boardName, id, boardId) => {
+        navigate(`/${boardName}/${id}`, {
+            state: {
+                page, sort, boardId, boardName
+            }
+        });
+    };
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                setSkeleton(true);
+                const response = await EP.getExposedPosts();
+                setPosts(response.data);
+
+            } catch(error) {
+                
+            } finally {
+                setSkeleton(false);
+            }
+        }
+        fetchPosts();
+    }, []);
+
+    return (
+        <>
+            {posts?.length > 0 && <S.Wrapper>
+                <S.TitleArea>
+                    <BC.Icon src={loudspeaker} $w={"15px"} />
+                    <S.Text $size={"16px"} $weight={"600"}>스포트라이트</S.Text>
+                    <S.TooltipWrapper>
+                        <BC.Icon src={information} />
+                        <S.TooltipText>
+                            <p style={{fontSize: "16px", fontWeight:"700", marginBottom:"5px"}}>스포트라이트란?</p>
+                            <p style={{fontSize:"14px"}}>사용자가 확성기 아이템을 사용해</p>
+                            <p style={{fontSize:"14px"}}>메인 페이지에 노출한 게시글입니다.</p>
+                            <p style={{fontSize:"14px"}}>노출은 사용 시점부터 24시간 유지됩니다.</p>
+                        </S.TooltipText>
+                    </S.TooltipWrapper>
+                </S.TitleArea>
+                    <S.Table>
+                        <colgroup>
+                            <col style={{ width: "15%" }} />
+                            <col style={{ width: "51%" }} />
+                            <col style={{ width: "12%" }} />
+                            <col style={{ width: "12%" }} />
+                            <col style={{ width: "10%" }} />
+                        </colgroup>
+                        <tbody>
+                            {skeleton ?
+                                Array.from({ length: 5 }).map((_, index) => (
+                                    <S.Row key={index}>
+                                        <S.FirstColumn><SkeletonItem $width="100%" $height="15px" $radius={"5px"} /></S.FirstColumn>
+                                        <S.Column><SkeletonItem $width="100%" $height="15px" $radius={"5px"} /></S.Column>
+                                        <S.Column><SkeletonItem $width="100%" $height="15px" $radius={"5px"} /></S.Column>
+                                        <S.Column><SkeletonItem $width="100%" $height="15px" $radius={"5px"} /></S.Column>
+                                        <S.Column><SkeletonItem $width="100%" $height="15px" $radius={"5px"} /></S.Column>
+                                    </S.Row>
+                                ))
+                                : posts.slice(0, 15).map((post) => (
+                                <S.Row key={post.postId}>
+                                    <S.FirstColumn><S.Text onClick={() => navigate(`/${post.boardName}`)} style={{cursor: "pointer"}}>[{post.boardKorean}]</S.Text></S.FirstColumn>
+                                    <S.Column $align={"left"} onClick={() => handleNavigatePost(post.boardName, post.postId, post.boardId)}><S.Text style={{cursor: "pointer"}}>{post.title}{post.commentCnt > 0 ? (<S.Comment>{post.commentCnt}</S.Comment>) : ""}</S.Text></S.Column>
+                                    <S.Column $align={"left"}><S.Text onClick={() => navigate(`/users/${post.userId}`)} style={{cursor: "pointer"}}>{post.userNickname}</S.Text></S.Column>
+                                    <S.Column><S.Text $color={"#878787"}>{formatDate(post.createdAt, 3)}</S.Text></S.Column>
+                                    <S.Column><S.Text $color={"#878787"}>{post.viewCnt}</S.Text></S.Column>
+                                </S.Row>
+                            ))}
+                        </tbody>
+                    </S.Table>
+            </S.Wrapper>}
+        </>
+    )
+}
+
+export default LoudSpeakerPost;
--- src/components/modal/inventoryItemUse/index.jsx
+++ src/components/modal/inventoryItemUse/index.jsx
@@ -4,15 +4,20 @@ import * as U from "@/apis/user";
 import * as UI from "@/apis/inventory";
 import { useEffect, useState } from "react";
 import { UserProfileImage } from "@/common/func/UserProfile";
+import useUserInfo from "@/hooks/localStorage";
 
 const InventoryUseModal = ({onClose, selectItem}) => {
+    const user = useUserInfo();
     const [targetId, setTargetId] = useState();
     const [targetName, setTargetName] = useState("");
-    const [userData, setUserData] = useState([]);
+    const [data, setData] = useState([]);
+
 
     const handleClickInventoryItem = async (item) => {
         try {
-            const response = await UI.useInventoryItem(item.userInventoryId, {"otherId": targetId});
+            const otherId = targetId;
+            const postId = targetId;
+            const response = await UI.useInventoryItem(item.userInventoryId, {otherId, postId});
             switch(response.data.type) {
                 case "ATTENDANCE_PLUS_TICKET" : {
                     alert(`아이템을 사용해서 연속 출석일 수가 ${response.data.data}로 증가하였습니다.`);
@@ -23,7 +28,15 @@ const InventoryUseModal = ({onClose, selectItem}) => {
                     break;
                 }
                 case "GLASS_GIFT_TICKET" : {
-                    alert(`아이템을 사용해서 님에게 성공적으로 유리 조각을 선물했습니다.`);
+                    alert(`아이템을 사용해서 ${response.data.data}님에게 성공적으로 유리 조각을 선물했습니다.`);
+                    break;
+                }
+                case "RANDOM_EMOTICON_BOX" : {
+                    alert(`아이템을 사용해서 성공적으로 ${response.data.data} 이모티콘을 획득했습니다.`);
+                    break;
+                }
+                case "LOUD_SPEAKER" : {
+                    alert(`아이템을 사용해서 성공적으로 ${response.data.data} 게시글을 메인 페이지에 띄웠습니다.`);
                     break;
                 }
                 default : {
@@ -37,24 +50,40 @@ const InventoryUseModal = ({onClose, selectItem}) => {
         }
     };
 
+    const fetchPostList = async () => {
+        try {
+            const response = await U.getUserPosts(user.userId, {page:0, sort: "createdAt", size: 100, direction: "DESC"});
+            setData(response.data.content);
+        } catch(error) {
+
+        }
+    };
+
     const fetchUserList = async () => {
         try {
             const response = await U.getUserList();
-            setUserData(response.data);
+            setData(response.data);
         } catch(error) {
 
         }
     };
 
     useEffect(() => {
-        fetchUserList();
+        if(selectItem.shopItemId === 402) {
+            fetchUserList();
+        }
+        if(selectItem.shopItemId === 405) {
+            fetchPostList();
+        }
     }, []);
     
     return (
         <>
             <S.Wrapper>
                 <S.Content>
-                    <S.Text $size={"18px"}><S.Text $size={"18px"} $weight={"600"}>{selectItem.name}</S.Text> 아이템을 {targetName ? `${targetName}님에게 `: ""}사용하시겠습니까?</S.Text>
+                    <S.Text $size={"18px"}><S.Text $size={"18px"} $weight={"600"}>{selectItem.name}</S.Text> 아이템을 {targetName ? `"${targetName}"에게 `: ""}사용하시겠습니까?</S.Text>
+                    {selectItem.description && <BC.Text $color={"#878787"}>{selectItem.description}</BC.Text>}
+                    <BC.Text></BC.Text>
                     <S.HorizontalWrapper>
                         <S.Button $bg={"#C6BC73"} style={{color: "white"}} onClick={() => handleClickInventoryItem(selectItem)}>사용</S.Button>
                         <S.Button $bg={"#E7E7E7"} onClick={onClose}>취소</S.Button>
@@ -65,13 +94,28 @@ const InventoryUseModal = ({onClose, selectItem}) => {
                         style={{backgroundColor: "white", width: "240px", maxHeight: "500px", boxShadow: "0px 0px 16px rgb(0, 0, 0, 0.5)", padding: "15px 25px", borderRadius: "10px", overflowY: "auto", cursor: "pointer"}}
                     >
                         <BC.Text $size={"15px"} $weight={"600"}>사용자 목록</BC.Text>
-                        {userData?.map((user) => (
+                        {data?.map((user) => (
                             <BC.HorizontalWrapper key={user.userId} $jc={"flex-start"} $gap={"10px"} style={{width: "100%", backgroundColor: targetId === user.userId ? "#eee" : "", padding: "5px", borderRadius: "8px", transition: "all 0.2s ease"}}
                                 onClick={() => {setTargetId(user.userId); setTargetName(user.nickName);}}
                             >
                                 <UserProfileImage user={user} height={"30px"} width={"30px"} radius={"30px"} />
                                 <BC.Text $size={"11px"} $weight={"600"} style={{backgroundColor: "#eee", borderRadius: "5px", padding: "1px 5px"}}>{user.role}</BC.Text>
                                 <BC.Text $size={"13px"}>{user.nickName}</BC.Text>
+                            </BC.HorizontalWrapper>
+                        ))}
+                    </BC.VerticalWrapper>
+                }
+                {selectItem.shopItemId === 405 &&
+                    <BC.VerticalWrapper $gap={"3px"} 
+                        style={{backgroundColor: "white", width: "300px", maxHeight: "500px", boxShadow: "0px 0px 16px rgb(0, 0, 0, 0.5)", padding: "15px 25px", borderRadius: "10px", overflowY: "auto", cursor: "pointer"}}
+                    >
+                        <BC.Text $size={"15px"} $weight={"600"}>작성 게시글 목록</BC.Text>
+                        {data?.map((post) => (
+                            <BC.HorizontalWrapper key={post.postId} $jc={"flex-start"} $gap={"10px"} style={{width: "100%", backgroundColor: targetId === post.postId ? "#eee" : "", padding: "5px", borderRadius: "8px", transition: "all 0.2s ease"}}
+                                onClick={() => {setTargetId(post.postId); setTargetName(post.title);}}
+                            >
+                                <BC.Text $size={"13px"} $weight={"600"}>[{post.boardKorean}]</BC.Text>
+                                <BC.Text $size={"13px"} style={{width: "50%", textOverflow: "ellipsis", overflowX: "hidden", whiteSpace: "nowrap"}}>{post.title}</BC.Text>
                             </BC.HorizontalWrapper>
                         ))}
                     </BC.VerticalWrapper>
